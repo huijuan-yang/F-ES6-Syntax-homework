d4d2b2b80f55663d06467f2f65cfab8c81db78bf
Huijuan Yang <huijuan.yang@Huijuans-MacBook-Pro.local>
2020-10-23 00:07:28 +0800

    completed two practices

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,15 @@
-const parseData
+const parseData = (input) => {
+    const {data, column} = input;
+    const result = [];
+
+    for (let each of data) {
+        const eachData = {};
+        column.map((eachColumn, index) => {
+            eachData[eachColumn.name] = each[index]
+        });
+        result.push(eachData);
+    }
+
+    return result;
+};
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,9 @@
-const inject
+const inject = (items, sections) => {
+    sections.sort((a, b) => a.index - b.index);
+    sections.map((eachSection, index) => {
+        items.splice(eachSection.index + index, 0, eachSection.content)
+    });
+
+    return items;
+}
 export { inject };
